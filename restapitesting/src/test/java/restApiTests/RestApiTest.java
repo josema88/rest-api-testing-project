82a3d243dd50e0ce897d3a2edeefab7e8b8fa617
José Manuel Ordoñez Castillo <jmordonez@domcoin.net>
2019-07-15 00:38:55 -0600

    Add Tests samples for OMDBApi

--- restapitesting/src/test/java/restApiTests/RestApiTest.java
+++ restapitesting/src/test/java/restApiTests/RestApiTest.java
@@ -1,11 +1,60 @@
 package restApiTests;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class RestApiTest {
 
+    String hostUrl = "http://www.omdbapi.com";
+    String getMovieByIdPath = "/?i={movie}&apikey={apikey}";
+    String getMoviesByNamePath = "?apikey={1}&s={2}";
+    String movieName = "Batman";
+    String movieId = "tt3896198";
+    String apiKey = "981288";
 
-    public RestApiTest(){
+
+
+    @BeforeMethod
+    public void InitializeTest(){
         System.out.println("Initialization");
+
+    }
+
+
+    @Test
+    public void TestMovieIdMatchName(){
+        RestAssured.baseURI = hostUrl;
+        RestAssured.port = 80;
+        System.out.println("Testing");
+        given().
+                pathParam("movie", movieId).
+                pathParam("apikey", apiKey).
+                when().
+                get(getMovieByIdPath).
+                then().
+                assertThat().
+                statusCode(200).
+                and().
+                contentType(ContentType.JSON).
+                body("Title", equalTo("Guardians of the Galaxy Vol. 2"));
+    }
+
+    @Test
+    public void TestBatmanForeverYear() {
+        RestAssured.baseURI = hostUrl;
+        RestAssured.port = 80;
+
+        System.out.println("Testing by Movie name, Movie: " + movieName);
+        given().
+                when().
+                get(getMoviesByNamePath, apiKey, movieName).
+                then().
+                body("Search.findAll { it.Title == 'Batman Forever' }.Year", hasItem("1995"));
 
     }
 
